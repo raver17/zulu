ef5a93165f407e1f0e46008e85b502dae060a278
raver17 <paulo.nextvas2024@gmail.com>
2025-04-14 08:40:19 +0800

    Update script.js

--- script.js
+++ script.js
@@ -56,4 +56,4 @@ window.addEventListener("resize", () => {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
-});
+}); 
